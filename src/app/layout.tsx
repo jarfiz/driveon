e4994af001982ac85aad99bedfc497dd1ea4af34
Jaenudin Firdaus <jaenudinfirdaus2004@gmail.com>
2025-09-29 20:00:17 +0700

    feat: initialize project with Tailwind CSS and create basic layout
    - Added PostCSS configuration for Tailwind CSS.
    - Created SVG icons for various UI elements.
    - Added favicon for the application.
    - Created global CSS file and imported Tailwind CSS.
    - Implemented root layout with metadata and Roboto font.
    - Created a simple homepage component.
    - Configured TypeScript with strict settings and Next.js support.

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Driveon",
+  description:
+    "DriveOn makes renting vehicles simple, fast, and reliable — anytime, anywhere.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.className} antialiased`}>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
